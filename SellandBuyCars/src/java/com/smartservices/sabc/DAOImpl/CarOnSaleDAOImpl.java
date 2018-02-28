0ce8e51adac9205e2af5e52a308f4b2b748de142
Prince Trivedi <prince-trivedi01@.github.com>
2018-02-28 14:53:11 +0530

    commit

--- SellandBuyCars/src/java/com/smartservices/sabc/DAOImpl/CarOnSaleDAOImpl.java
+++ SellandBuyCars/src/java/com/smartservices/sabc/DAOImpl/CarOnSaleDAOImpl.java
@@ -7,7 +7,12 @@ package com.smartservices.sabc.DAOImpl;
 
 import com.smartservices.sabc.DAO.CarOnSaleDAO;
 import com.smartservices.sabc.entities.Caronsale;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,11 +22,30 @@ public class CarOnSaleDAOImpl implements CarOnSaleDAO{
 
     @Override
     public int addCarOnSale(Caronsale Caronsale) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int count=0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement("Insert into Caronsale (model,name,regno,milage,condition,status,amount) values (?,?,?,?,?,?,?)");
+            ps.setString(1,Caronsale.getModel());
+            ps.setString(2,Caronsale.getName());
+            ps.setString(3,Caronsale.getRegno());
+            ps.setString(4,Caronsale.getMilage());
+            ps.setString(5,Caronsale.getCondition());
+            ps.setString(6,Caronsale.getStatus());
+            ps.setInt(7,Caronsale.getAmount());
+            
+            count = ps.executeUpdate();
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(CarOnSaleDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+        return count;
     }
 
     @Override
-    public int deleteCarOnSale(int cosaid) {
+    public int deleteCarOnSale(int cosaId) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -31,12 +55,12 @@ public class CarOnSaleDAOImpl implements CarOnSaleDAO{
     }
 
     @Override
-    public Caronsale getCarOnSaleByID(int cosaid) {
+    public Caronsale getCarOnSaleByID(int cosaId) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public int updateCarOnSale(int cosaid, Caronsale Caronsale) {
+    public int updateCarOnSale(int cosaId, Caronsale Caronsale) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
--- SellandBuyCars/src/java/com/smartservices/sabc/DAOImpl/CarOnShowroomDAOImpl.java
+++ SellandBuyCars/src/java/com/smartservices/sabc/DAOImpl/CarOnShowroomDAOImpl.java
@@ -7,7 +7,12 @@ package com.smartservices.sabc.DAOImpl;
 
 import com.smartservices.sabc.DAO.CarOnShowroomDAO;
 import com.smartservices.sabc.entities.Caronshowroom;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,7 +22,21 @@ public class CarOnShowroomDAOImpl implements CarOnShowroomDAO{
 
     @Override
     public int addCarOnShowroom(Caronshowroom Caronshowroom) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int count=0;
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement("Insert into Caronshowroom (model,name,type,amount,color) values (?,?,?,?,?)");
+            ps.setString(1,Caronshowroom.getModel());
+            ps.setString(2,Caronshowroom.getName());
+            ps.setString(3,Caronshowroom.getType());
+            ps.setInt(4,Caronshowroom.getAmount());
+            ps.setString(5,Caronshowroom.getColor());
+            
+            count = ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(CarOnShowroomDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return count;
     }
 
     @Override
